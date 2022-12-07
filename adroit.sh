c4d1b3eb5cac6a8d75f7189399be43e524d4c17b
Enes Erciyes <m.eneserciyes@gmail.com>
2022-12-07 15:26:16 +0300

    slurms + wandb online

--- adroit.sh
+++ adroit.sh
@@ -3,11 +3,11 @@ export PYTHONPATH=.:$PYTHONPATH
 name=T-2
 datasets=(pen-cloned-v0)
 
-for round in {1..5}; do
+for round in {1..2}; do
   for data in ${datasets[@]}; do
     python scripts/train.py --dataset $data --exp_name $name-$round --tag development --seed $round
     python scripts/trainprior.py --dataset $data --exp_name $name-$round
-    for i in {1..20};
+    for i in {1..5};
     do
       python scripts/plan.py --test_planner beam_prior --dataset $data --exp_name $name-$round --suffix $i --n_expand 4 --beam_width 256 --horizon 24
     done
--- loco.sh
+++ loco.sh
@@ -3,11 +3,11 @@ export PYTHONPATH=.:$PYTHONPATH
 name=T-1
 datasets=(hopper-medium-replay-v2 hopper-medium-expert-v2)
 
-for round in {1..5}; do
+for round in {1..2}; do
   for data in ${datasets[@]}; do
     python scripts/train.py --dataset $data --exp_name $name-$round --tag development --seed $round
     python scripts/trainprior.py --dataset $data --exp_name $name-$round
-    for i in {1..20};
+    for i in {1..5};
     do
        python scripts/plan.py --test_planner beam_prior --dataset $data --exp_name $name-$round --suffix $i --beam_width 64 --n_expand 4 --horizon 15
     done
@@ -15,7 +15,7 @@ for round in {1..5}; do
 done
 
 for data in ${datasets[@]}; do
-  for round in {1..5}; do
+  for round in {1..2}; do
     python plotting/read_results.py --exp_name $name-$round --dataset $data
   done
   python plotting/read_results.py --exp_name $name --dataset $data
