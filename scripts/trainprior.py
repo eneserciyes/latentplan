c73be32ef095c11c681c4b6f53ed50abc7ed88e7
eneserciyes <m.eneserciyes@gmail.com>
2022-12-08 23:20:03 +0300

    wandb offline

--- scripts/trainprior.py
+++ scripts/trainprior.py
@@ -102,7 +102,7 @@ trainer = trainer_config()
 ## scale number of epochs to keep number of updates constant
 n_epochs = 1 # int(1e6 / len(dataset) * args.n_epochs_ref)
 save_freq = 1 # int(n_epochs // args.n_saves)
-wandb.init(project="latentPlanning", config=args, tags=[args.exp_name, args.tag, "prior"])
+wandb.init(project="latentPlanning", config=args, tags=[args.exp_name, args.tag, "prior"], mode="offline")
 
 for epoch in range(n_epochs):
     print(f'\nEpoch: {epoch} / {n_epochs} | {args.dataset} | {args.exp_name}')
