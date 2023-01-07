1919fecee88cd0097014bbc5ef3536a53ea5c22a
eneserciyes <m.eneserciyes@gmail.com>
2023-01-07 22:59:45 +0000

    parallelize adroit training

--- adroit.sh
+++ adroit.sh
@@ -3,7 +3,8 @@ export PYTHONPATH=.:$PYTHONPATH
 name=T-2
 datasets=(pen-cloned-v0)
 
-for round in {1..5}; do
+foo() {
+  local round=$1
   for data in ${datasets[@]}; do
     python scripts/train.py --dataset $data --exp_name $name-$round --tag development --seed $round
     python scripts/trainprior.py --dataset $data --exp_name $name-$round
@@ -12,12 +13,16 @@ for round in {1..5}; do
       python scripts/plan.py --test_planner beam_prior --dataset $data --exp_name $name-$round --suffix $i --n_expand 4 --beam_width 256 --horizon 24
     done
   done
+}
+
+for round in {1..5}; do
+  foo "$round" &
 done
 
-for data in ${datasets[@]}; do
-  for round in {1..1}; do
-    python plotting/read_results.py --exp_name $name-$round --dataset $data
-  done
-  python plotting/read_results.py --exp_name $name --dataset $data
-done
+# for data in ${datasets[@]}; do
+#   for round in {1..1}; do
+#     python plotting/read_results.py --exp_name $name-$round --dataset $data
+#   done
+#   python plotting/read_results.py --exp_name $name --dataset $data
+# done
 
